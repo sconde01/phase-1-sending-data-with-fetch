cd3215cdfcf45a84e62b70e5b7ecf403af9819e0
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-07-20 07:44:44 -0700

    commit tree 0fef3274b7d2641ba47bed67f53169f4a31f520c

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const submitData = (name, email) => {
+  const configurationObject ={
+    method:'POST',
+    headers: {
+      'content-type': "application/json",
+      "accept": "application/json",
+    },
+    body: JSON.stringify({name, email})
+    
+  };
+  return fetch ("http://localhost:3000/users", configurationObject)
+  .then(resp => resp.json())
+  .then(body => addNewContact (body.id))
+  //  .then (data => {
+  // console.log(data)
+  // const body = document.querySelector (body)
+  // body.append(data.id)
+  .catch(response => "Unauthorized Access")
+
+}
+
+const addNewContact = (newID) => {
+  document.body.innerHTML = newID;
+  document.body.append(message)
+}
+
